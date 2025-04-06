03c38c431e06d5b38943c92be8376d3a29afa58b
bodedeneal <denealbode@gmail.com>
2025-04-06 11:03:43 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+ var youtube = document.createElement('h2');
+    youtube.textContent = "  *  ";
+    youtube.textContent = "*****";
+    youtube.textContent = "  *  ";
+    youtube.textContent = "  *  "; 
+   document.body.appendChild(h2Working);
