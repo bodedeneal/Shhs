7fee728cc6c0fcab2e4f5b8a800ca4fe8df2ba90
bodedeneal <denealbode@gmail.com>
2025-04-06 11:11:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
- var youtube = document.createElement('h2');
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.backgroundColor = "#000000"; // Neon black background
+    document.body.style.color = "black"; // Neon green text
+    document.body.style.fontFamily = "'Arial', sans-serif"; // Change this to your desired font
+    document.body.style.fontWeight = "bold"; // Make the text bold
+    document.body.style.fontSize = "16px";
+
+var youtube = document.createElement('h2');
     youtube.textContent = "  *  ";
     youtube.textContent = "*****";
     youtube.textContent = "  *  ";
